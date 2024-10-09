63aea3480ccfdf6fb951a11a99e055a860a5060b
Your Name <you@example.com>
2024-10-09 23:27:00 +0530

    Admin dashboard Updated Code

--- adminuse/urls.py
+++ adminuse/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import addTrainer
+from .views import addTrainer,adminDashboard
 urlpatterns = [
-    path('trainer_view/', addTrainer, name="add_trainer")
+    path('trainer_view/', addTrainer, name="add_trainer"),
+    path('admin_dashboard/', adminDashboard, name="admin_dashboard"),
 ]
--- adminuse/views.py
+++ adminuse/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import AddTrainers
+from courses.models import Category
 # Create your views here.
 def addTrainer(request):
     return render(request, 'courses/add_trainer.html')
 
+def adminDashboard(request):
+    categories = Category.objects.all()
+    return render(request, 'adminuse/admin.html',{'categories':categories})
