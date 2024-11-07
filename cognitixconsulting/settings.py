d213e3878ac5dd8d7f44b23bf2870ab0a9ed2525
Your Name <you@example.com>
2024-11-07 18:18:11 +0530

    Initial Commit

--- cognitixconsulting/settings.py
+++ cognitixconsulting/settings.py
@@ -110,8 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Asia/Kolkata'
 
-TIME_ZONE = 'Asia/kolkata'
 
 USE_I18N = True
 
