d810d54ebab502004b94a4d1af4cfc4b97081b66
Your Name <you@example.com>
2024-11-08 07:59:13 +0530

    make it better

--- cognitixconsulting/settings.py
+++ cognitixconsulting/settings.py
@@ -139,7 +139,7 @@ MEDIA_URL = "/media/"
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
