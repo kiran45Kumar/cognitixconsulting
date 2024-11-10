d40f3f6df5fa7391d42a36cd63a728ace3cf43cb
yashas281529 <116624803+yashas281529@users.noreply.github.com>
2024-11-10 14:12:21 +0530

    Update settings.py

--- cognitixconsulting/settings.py
+++ cognitixconsulting/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3n*7a&5c4!l)y82ni59pa5@0n#&h&n32jz#r=hu48c6)-=)qj!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ["cognitixconsulting.com","www.cognitixconsulting.com"]
 
